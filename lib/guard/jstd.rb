4497f5cbefd45b04d9eb230aa589de87a2c8c399
aRailsDemo <arailsdemo@gmail.com>
2011-04-06 15:49:42 -0700

    Revisiting The Jstd Class

--- lib/guard/jstd.rb
+++ lib/guard/jstd.rb
@@ -6,5 +6,17 @@ module Guard
     autoload :Runner, 'guard/jstd/runner'
     autoload :CaseFinder, 'guard/jstd/case_finder'
 
+    def start
+      UI.info "Guard::Jstd is running."
+    end
+
+    def run_all
+      Runner.run
+    end
+
+    def run_on_change(paths)
+      cases = CaseFinder.find(paths)
+      Runner.run(cases)
+    end
   end
 end
--- spec/guard/jstd_spec.rb
+++ spec/guard/jstd_spec.rb
@@ -6,4 +6,26 @@ describe Guard::Jstd do
   it "inherits from Guard" do
     klass.ancestors.should include Guard::Guard
   end
+
+  it "#start sends a message to UI" do
+    ::Guard::UI.should_receive(:info).with(
+      "Guard::Jstd is running."
+    )
+    subject.start
+  end
+
+  it " #run_all runs all tests" do
+    klass::Runner.should_receive(:run)
+    subject.run_all
+  end
+
+  describe "#run_on_change" do
+    it "starts the Runner with the corresponding TestCases" do
+      paths = ['foo/bar.js', 'hand/foot.js']
+      cases = 'First,Second'
+      klass::CaseFinder.should_receive(:find).with(paths) { cases }
+      klass::Runner.should_receive(:run).with(cases)
+      subject.run_on_change(paths)
+    end
+  end
 end
