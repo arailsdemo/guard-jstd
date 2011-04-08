b3e7b21c5c4795555f567e320b488ff352c3dcda
aRailsDemo <arailsdemo@gmail.com>
2011-04-08 15:39:07 -0700

    Finished Configuration

--- lib/guard/jstd/configuration.rb
+++ lib/guard/jstd/configuration.rb
@@ -2,30 +2,40 @@ module Guard
   class Jstd
     module Configuration
       class << self
-        attr_writer :java_path, :browser_paths, :server_port,
-                    :jstd_config_path
+        METHODS = [:java_path, :browser_paths, :server_port,
+                    :jstd_config_path, :start_server, :capture_browser]
+
+        METHODS.each do |method|
+          attr_writer method
+        end
 
         def reset!
-          @java_path = nil
-          @browser_paths = nil
-          @server_port = nil
-          @jstd_config_path = nil
+          METHODS.each { |method| instance_variable_set("@#{method.to_s}", nil) }
+          @jar_home = nil
         end
 
         def java_path
-          @java_path ||= '$JSTESTDRIVER_HOME/JsTestDriver-1.3.2.jar'
+          @java_path ||= jar_path
         end
 
         def browser_paths
           @browser_paths ||= "\`which open\`"
         end
 
+       def jstd_config_path
+          @jstd_config_path ||= 'jsTestDriver.conf'
+        end
+
         def server_port
           @server_port ||= default_server_port
         end
 
-        def jstd_config_path
-          @jstd_config_path ||= 'jsTestDriver.conf'
+        def start_server
+          @start_server == nil ? true : @start_server
+        end
+
+        def capture_browser
+          @capture_browser == nil ? false : @capture_browser
         end
 
         def default_server_port
@@ -35,6 +45,25 @@ module Guard
           rescue
             '4224'
           end
+        end
+
+        def jar_path
+          path = File.join(jar_home, 'jstest*.jar')
+          files = Dir.glob(File.expand_path(path), File::FNM_CASEFOLD)
+          if files.empty?
+            alert = "\e[31mGuard::Jstd could not find your JsTestDriver.jar file. "
+            alert << "Please specify it in the configuration.\e[0m"
+            UI.info(alert, :reset => true)
+            exit
+          else
+            files.first
+          end
+        end
+
+        def jar_home
+          return @jar_home if @jar_home
+          echo = `echo $JSTESTDRIVER_HOME`
+          @jar_home = echo == "" ? '~/bin' : echo.gsub!("\n", "")
         end
       end
     end
--- spec/guard/jstd/configuration_spec.rb
+++ spec/guard/jstd/configuration_spec.rb
@@ -7,36 +7,35 @@ describe Guard::Jstd::Configuration do
     subject.reset!
   end
 
-  describe ".reset!" do
-    it "sets the java_path to the default" do
-      subject.java_path = 'foo/bar.jar'
-      subject.java_path.should == 'foo/bar.jar'
-      subject.reset!
-
-      subject.java_path.should == '$JSTESTDRIVER_HOME/JsTestDriver-1.3.2.jar'
+  describe ".jar_home" do
+    it "uses $JSTESTDRIVER_HOME when present" do
+      dir = "path\n"
+      subject.should_receive("`").with(
+        "echo $JSTESTDRIVER_HOME"
+      ).and_return(dir)
+      subject.jar_home.should == "path"
     end
 
-    it "sets the browser_paths to the default" do
-      subject.browser_paths = 'path'
-      subject.browser_paths.should == 'path'
-      subject.reset!
+    it "uses the default when $JSTESTDRIVER_HOME isn't present" do
+      subject.should_receive("`").with(
+        "echo $JSTESTDRIVER_HOME"
+      ).and_return("")
+      subject.jar_home.should == '~/bin'
+    end
+  end
 
-      subject.browser_paths.should == "\`which open\`"
+  describe ".jar_path" do
+    before { subject.stub(:jar_home).and_return("/path") }
+
+    it "finds the .jar file in the jar_home" do
+      Dir.should_receive(:glob).with("/path/jstest*.jar", File::FNM_CASEFOLD) { ["file.jar"] }
+      subject.jar_path.should == "file.jar"
     end
 
-    it "sets the server_port to the default" do
-      subject.stub(:default_server_port) { 123 }
-      subject.reset!
-
-      subject.server_port.should == 123
-    end
-
-    it "sets the jstd_config_path to the default" do
-      subject.jstd_config_path = 'hi'
-      subject.jstd_config_path.should == 'hi'
-      subject.reset!
-
-      subject.jstd_config_path.should == 'jsTestDriver.conf'
+    it "exits if the .jar file isn't found" do
+      Dir.should_receive(:glob).with("/path/jstest*.jar", File::FNM_CASEFOLD) { [] }
+      Guard::UI.should_receive(:info)
+      expect { subject.jar_path }.to raise_error
     end
   end
 
@@ -61,12 +60,59 @@ load:
       subject.default_server_port.should == "2345"
     end
 
-    it "does not raise an error if there is no configuration file" do
-      expect { subject.default_server_port }.to_not raise_exception
-    end
-
     it "returns 4224 if there is no configuration file" do
       subject.default_server_port.should == "4224"
+    end
+  end
+
+  describe ".reset!" do
+    it "sets the java_path to the default" do
+      subject.stub(:jar_path) { "home" }
+
+      subject.java_path = 'foo/bar.jar'
+      subject.java_path.should == 'foo/bar.jar'
+      subject.reset!
+
+      subject.java_path.should == 'home'
+    end
+
+    it "sets the browser_paths to the default" do
+      subject.browser_paths = 'path'
+      subject.browser_paths.should == 'path'
+      subject.reset!
+
+      subject.browser_paths.should == "\`which open\`"
+    end
+
+    it "sets the server_port to the default" do
+      subject.stub(:default_server_port) { 123 }
+      subject.reset!
+
+      subject.server_port.should == 123
+    end
+
+    it "sets the jstd_config_path to the default" do
+      subject.jstd_config_path = 'hi'
+      subject.jstd_config_path.should == 'hi'
+      subject.reset!
+
+      subject.jstd_config_path.should == 'jsTestDriver.conf'
+    end
+
+    it "sets the start_server option to true" do
+      subject.start_server = false
+      subject.start_server.should == false
+      subject.reset!
+
+      subject.start_server.should == true
+    end
+
+    it "sets the capture_browser option to false" do
+      subject.capture_browser = true
+      subject.capture_browser.should == true
+      subject.reset!
+
+      subject.capture_browser.should == false
     end
   end
 end
